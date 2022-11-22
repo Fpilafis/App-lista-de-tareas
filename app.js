c0d13ec725c232ba23281291e5fc03cf6c76fb45
Fpilafis <fpilafis@hotmail.com>
2022-11-22 10:34:50 -0300

    Reescribiendo parte del codigo html

--- app.js
+++ app.js
@@ -16,9 +16,6 @@ let tareasPronto = {}
 let tareasUrgente = {}
 
 
-//////////////////////////////////////////////////////////////---------------------------//////////////////////////////////////////////////////////
-
-
 //LOCAL STORAGE
 document.addEventListener('DOMContentLoaded', () => {
     if (localStorage.getItem('tareas')) {
@@ -43,11 +40,11 @@ listas.addEventListener('click', (e) => {
 })
 
 botones.addEventListener('click', e => {
-    setTareaRelax(e)
+    setTarea(e)
 })
 
-//FUNCIONES
-const setTareaRelax = e => {
+//FUNCIONES setTarea y btnAccion
+const setTarea = e => {
     const texto = placeholder.value
     
     texto.length > 120 ? (
@@ -57,24 +54,26 @@ const setTareaRelax = e => {
     : texto.trim() === '' ? (
         placeholder.setAttribute("placeholder", "Agrega una tarea valida"))
     : e.target.classList.contains('btn-urgente') ? (
-        tareaUrgente = {
+        tarea = {
             id: Date.now(),
             texto: texto,
-            estado: false
+            estado: false,
+            btn: "urgente"
         },
         placeholder.setAttribute("placeholder", "Ingrese tarea"),
-        tareasUrgente[tareaUrgente.id] = tareaUrgente,
+        tareasUrgente[tarea.id] = tarea,
         pintarTareasUrgente(),
         formulario.reset()
         )
     : e.target.classList.contains('btn-pronto') ? (
-        tareaPronto = {
+        tarea = {
             id: Date.now(),
             texto: texto,
-            estado: false
+            estado: false,
+            btn: "pronto"
         },
         placeholder.setAttribute("placeholder", "Ingrese tarea"),
-        tareasPronto[tareaPronto.id] = tareaPronto,
+        tareasPronto[tarea.id] = tarea,
         pintarTareasPronto(),
         formulario.reset()
         )
@@ -82,7 +81,8 @@ const setTareaRelax = e => {
         tarea = {
             id: Date.now(),
             texto: texto,
-            estado: false
+            estado: false,
+            btn: "relax"
         },
         placeholder.setAttribute("placeholder", "Ingrese tarea"),
         tareas[tarea.id] = tarea,
@@ -90,38 +90,6 @@ const setTareaRelax = e => {
         formulario.reset()
         ) : console.log("a")
     }
-
-
-const pintarTareas = () => {
-    
-    localStorage.setItem('tareas', JSON.stringify(tareas))
-
-    if (Object.values(tareas).length === 0) {
-        listaTareas.innerHTML = `
-        <div class="alert alert-primary text-center">
-        <strong><p class="h8"><u>Relax</u></p>Sin tareas pendientes</strong>
-        </div>
-        `
-        return
-    }
-    
-    listaTareas.innerHTML = ''
-
-    Object.values(tareas).forEach(tarea => {
-        const clone = template.cloneNode(true)
-        clone.querySelector('p').textContent = tarea.texto
-        if (tarea.estado) {
-            clone.querySelectorAll('.fas')[0].classList.replace('fa-check-circle', 'fa-undo-alt')
-            clone.querySelector('.alert').classList.replace('alert-primary', 'alert-dark')
-            clone.querySelector('p').style.textDecoration = 'line-through'
-        }
-        clone.querySelectorAll('.fas')[0].dataset.id = tarea.id
-        clone.querySelectorAll('.fas')[1].dataset.id = tarea.id
-        fragment.appendChild(clone)
-    })
-
-    listaTareas.appendChild(fragment)
-}
 
 const btnAccion = e => {
     if (e.target.classList.contains("lista-tareas")){
@@ -173,8 +141,42 @@ const btnAccion = e => {
 
 //////////////////////////////////////////////////////////////---------------------------//////////////////////////////////////////////////////////
 
-//LISTA PRONTO
+// PINTAR LISTA RELAX
+const pintarTareas = () => {
+    
+    localStorage.setItem('tareas', JSON.stringify(tareas))
 
+    if (Object.values(tareas).length === 0) {
+        listaTareas.innerHTML = `
+        <div class="alert alert-primary text-center">
+        <strong><p class="h8"><u>Relax</u></p>Sin tareas pendientes</strong>
+        </div>
+        `
+        return
+    }
+    
+    listaTareas.innerHTML = ''
+
+    Object.values(tareas).forEach(tarea => {
+        const clone = template.cloneNode(true)
+        clone.querySelector('p').textContent = tarea.texto
+
+        if (tarea.estado) {
+            clone.querySelectorAll('.fas')[0].classList.replace('fa-check-circle', 'fa-undo-alt')
+            clone.querySelector('.alert').classList.replace('alert-primary', 'alert-dark')
+            clone.querySelector('p').style.textDecoration = 'line-through'
+        }
+
+        clone.querySelectorAll('.fas')[0].dataset.id = tarea.id
+        clone.querySelectorAll('.fas')[1].dataset.id = tarea.id
+        fragment.appendChild(clone)
+    })
+    listaTareas.appendChild(fragment)
+}
+
+//////////////////////////////////////////////////////////////---------------------------//////////////////////////////////////////////////////////
+
+// PINTAR LISTA PRONTO
 const pintarTareasPronto = () => {
     
     localStorage.setItem('tareass', JSON.stringify(tareasPronto))
@@ -190,26 +192,26 @@ const pintarTareasPronto = () => {
     
     listaTareasPronto.innerHTML = ''
 
-    Object.values(tareasPronto).forEach(tareaPronto => {
+    Object.values(tareasPronto).forEach(tarea => {
         const clone = templateTwo.cloneNode(true)
-        clone.querySelector('p').textContent = tareaPronto.texto
-        if (tareaPronto.estado) {
+        clone.querySelector('p').textContent = tarea.texto
+
+        if (tarea.estado) {
             clone.querySelectorAll('.fas')[0].classList.replace('fa-check-circle', 'fa-undo-alt')
             clone.querySelector('.alert').classList.replace('alert-success', 'alert-dark')
             clone.querySelector('p').style.textDecoration = 'line-through'
         }
-        clone.querySelectorAll('.fas')[0].dataset.id = tareaPronto.id
-        clone.querySelectorAll('.fas')[1].dataset.id = tareaPronto.id
+
+        clone.querySelectorAll('.fas')[0].dataset.id = tarea.id
+        clone.querySelectorAll('.fas')[1].dataset.id = tarea.id
         fragment.appendChild(clone)
     })
-
     listaTareasPronto.appendChild(fragment)
 }
 
 //////////////////////////////////////////////////////////////---------------------------//////////////////////////////////////////////////////////
 
-// LISTA URGENTE
-
+// PINTAR LISTA URGENTE
 const pintarTareasUrgente = () => {
 
     localStorage.setItem('tareasss', JSON.stringify(tareasUrgente))
@@ -225,19 +227,19 @@ const pintarTareasUrgente = () => {
     
     listaTareasUrgente.innerHTML = ''
 
-    Object.values(tareasUrgente).forEach(tareaUrgente => {
+    Object.values(tareasUrgente).forEach(tarea => {
         const clone = templatethree.cloneNode(true)
-        clone.querySelector('p').textContent = tareaUrgente.texto
-        if (tareaUrgente.estado) {
+        clone.querySelector('p').textContent = tarea.texto
+
+        if (tarea.estado) {
             clone.querySelectorAll('.fas')[0].classList.replace('fa-check-circle', 'fa-undo-alt')
             clone.querySelector('.alert').classList.replace('alert-danger', 'alert-dark')
             clone.querySelector('p').style.textDecoration = 'line-through'
         }
-        clone.querySelectorAll('.fas')[0].dataset.id = tareaUrgente.id
-        clone.querySelectorAll('.fas')[1].dataset.id = tareaUrgente.id
+
+        clone.querySelectorAll('.fas')[0].dataset.id = tarea.id
+        clone.querySelectorAll('.fas')[1].dataset.id = tarea.id
         fragment.appendChild(clone)
     })
-    
     listaTareasUrgente.appendChild(fragment)
 }
-
